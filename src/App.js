b20abf9afc6e033f98b47d8aba85f287085f6ab2
Mariam Fatima <mariam@marax.in>
2018-09-19 14:46:22 +0530

    Created and integrated MarketOverview Component with a responsive table inside App Component.

--- src/App.js
+++ src/App.js
@@ -28,7 +28,52 @@ class Navigation extends Component {
 							<MenuItem eventKey={3.3}>all</MenuItem>
 						</NavDropdown>
 					</Nav>
-				</Navbar>;
+				</Navbar>
+			</div>
+		);
+	}
+}
+
+class MarketOverview extends Component {
+	render() {
+		return (
+			<div className="Table">
+				<Table 
+					responsive
+					bordered
+					condensed
+					hover
+					striped
+				>
+					<thead>
+						<tr>
+							<th>Rank</th>
+							<th>Name</th>
+							<th>Price</th>
+							<th>Price Change (24th)</th>
+							<th>Market Cap</th>
+							<th>Volume (24th)</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>1</td>
+							<td>Bitcoin</td>
+							<td>$6,764.99</td>
+							<td>-4.41%</td>
+							<td>$114,784,460,451</td>
+							<td>$4,551,630,000</td>
+						</tr>
+						<tr>
+							<td>2</td>
+							<td>Ethereum</td>
+							<td>$395.74</td>
+							<td>-1.04%</td>
+							<td>$39,064,506,094</td>
+							<td>$1,367,120,000</td>
+						</tr>
+					</tbody>
+				</Table>
 			</div>
 		);
 	}
@@ -37,8 +82,9 @@ class Navigation extends Component {
 class App extends Component {
   render() {
     return (
-      	<div className="App">
+      	<div>
 			<Navigation></Navigation>
+			<MarketOverview></MarketOverview>
       	</div>
     );
   }
