7fad48b10340449d0805a166297aafc113e6b227
Mariam Fatima <mariam@marax.in>
2018-09-19 12:43:54 +0530

    Created the navbar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Navbar} from 'react-bootstrap';
+import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <a href="#">Top Coins</a>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-        </Navbar>;
-      </div>
+      	<div className="App">
+			<Navbar>
+				<Navbar.Header>
+					<Navbar.Brand>
+					<a href="#">Top Coins</a>
+					</Navbar.Brand>
+					<Navbar.Toggle />
+				</Navbar.Header>
+			<Nav>
+				<NavItem eventKey={1} href="#">
+					Market Overview
+				</NavItem>
+				<NavItem eventKey={2} href="#">
+					Liquidity Analysis Page
+				</NavItem>
+			</Nav>
+			<Nav pullRight>
+				<NavDropdown eventKey={3} title="Coin Limit" id="basic-nav-dropdown">
+					<MenuItem eventKey={3.1}>10</MenuItem>
+					<MenuItem eventKey={3.2}>50</MenuItem>
+					<MenuItem eventKey={3.3}>all</MenuItem>
+				</NavDropdown>
+			</Nav>
+			</Navbar>;
+      	</div>
     );
   }
 }
