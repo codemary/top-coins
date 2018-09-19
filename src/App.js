dc99c5425449f5095430720b4ea68b0f3bf46984
Mariam Fatima <mariam@marax.in>
2018-09-19 21:38:45 +0530

    Integrated react router with MarketOverview and LiquidityAnalysis.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/navigation';
-import MarketOverview from './components/market_overview';
-import LiquidityAnalysis from './components/liquidity_analysis';
 
 // parent component
 class App extends Component {
   render() {
     return (
-      	<div>
-			<Navigation/>
-			<MarketOverview/>
-			<LiquidityAnalysis/>
-      	</div>
+		<Navigation/>
     );
   }
 }
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,33 +1,43 @@
 import React, { Component } from 'react';
-import {Navbar, Nav, NavItem, NavDropdown, MenuItem, Table} from 'react-bootstrap';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import MarketOverview from './market_overview';
+import LiquidityAnalysis from './liquidity_analysis';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class Navigation extends Component {
     render() {
         return (
             <div>
-                <Navbar>
-					<Navbar.Header>
-						<Navbar.Brand>
-						<a href="#">Top Coins</a>
-						</Navbar.Brand>
-						<Navbar.Toggle />
-					</Navbar.Header>
-					<Nav>
-						<NavItem eventKey={1} href="#">
-							Market Overview
-						</NavItem>
-						<NavItem eventKey={2} href="#">
-							Liquidity Analysis Page
-						</NavItem>
-					</Nav>
-					<Nav pullRight>
-						<NavDropdown eventKey={3} title="Coin Limit" id="basic-nav-dropdown">
-							<MenuItem eventKey={3.1}>10</MenuItem>
-							<MenuItem eventKey={3.2}>50</MenuItem>
-							<MenuItem eventKey={3.3}>all</MenuItem>
-						</NavDropdown>
-					</Nav>
-				</Navbar>
+                <Router>
+                    <div>
+                        {/* header */}
+                        <Navbar>
+                            <Navbar.Header>
+                                <Navbar.Brand>
+                                    <a href="/">Top Coins</a>
+                                </Navbar.Brand>
+                                <Navbar.Toggle />
+                            </Navbar.Header>
+                            <Nav>
+                                <NavItem eventKey={1} href="#">
+                                    <Link to="/">Market Overview</Link>
+                                </NavItem>
+                                <NavItem eventKey={2} href="#">
+                                    <Link to="/liquidity">Liquidity Analysis</Link>
+                                </NavItem>
+                            </Nav>
+                            <Nav pullRight>
+                                <NavDropdown eventKey={3} title="Coin Limit" id="basic-nav-dropdown">
+                                    <MenuItem eventKey={3.1}>10</MenuItem>
+                                    <MenuItem eventKey={3.2}>50</MenuItem>
+                                    <MenuItem eventKey={3.3}>all</MenuItem>
+                                </NavDropdown>
+                            </Nav>
+                        </Navbar>
+                        <Route exact path="/" component={MarketOverview} />
+                        <Route path="/liquidity" component={LiquidityAnalysis} />
+                    </div>
+                </Router>
             </div>
         );
     }
