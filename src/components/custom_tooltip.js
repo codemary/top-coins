4b2c5641decb3298d068318f666823c94fc1aa97
Mariam Fatima <mariam@marax.in>
2018-09-20 04:24:51 +0530

    Add signs to tooltip fields

--- src/components/custom_tooltip.js
+++ src/components/custom_tooltip.js
@@ -11,9 +11,9 @@ class CustomTooltip extends Component {
             return (
                 <div style={{padding: 10, backgroundColor:"#DDDCE2", fontSize: 11}}>
                     <p>Name: {info.coin}</p>
-                    <p>Market Cap: {info.x}</p>
-                    <p>Volume: {info.y}</p>
-                    <p>Price Change: {info.z}</p>
+                    <p>Market Cap: ${info.x}</p>
+                    <p>Volume: ${info.y}</p>
+                    <p>Price Change: {info.z}%</p>
                 </div>
             );
         }
