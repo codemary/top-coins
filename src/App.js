211cb22a8251d16a245ab2e810692ba482fd38cd
Mariam Fatima <mariam@marax.in>
2018-09-20 10:57:47 +0530

    1. Removed p tag from coin limit navitem to fix height of navbar 2. Formatted App.js

--- src/App.js
+++ src/App.js
@@ -9,115 +9,115 @@ import fetch from 'isomorphic-fetch';
 
 class App extends Component {
 
-    state = {
-        market_overview_data: [],
-        liquidity_data: [],
-        limit: 10,
-    }
+	state = {
+		market_overview_data: [],
+		liquidity_data: [],
+		limit: 10,
+	}
 
-    fetchTickerData = () => {
-        let liquidity_data = [];
-        let market_overview_data = [];
+	fetchTickerData = () => {
+		let liquidity_data = [];
+		let market_overview_data = [];
 
-        fetch('https://api.coinmarketcap.com/v2/ticker/?&limit=' + this.state.limit)
-        .then(response => response.json())
-            .then(ticker_data => {
+		fetch('https://api.coinmarketcap.com/v2/ticker/?&limit=' + this.state.limit)
+			.then(response => response.json())
+			.then(ticker_data => {
 
-                // transform coin api data
-                let coin_data = []
-                Object.keys(ticker_data.data).forEach(key => {
-                    let val = ticker_data.data[key];
-                    coin_data.push(val)
-                });
+				// transform coin api data
+				let coin_data = []
+				Object.keys(ticker_data.data).forEach(key => {
+					let val = ticker_data.data[key];
+					coin_data.push(val)
+				});
 
-                // sort coins by rank
-                let coin_data_sorted = coin_data.sort((x,y)=>x.rank-y.rank)
+				// sort coins by rank
+				let coin_data_sorted = coin_data.sort((x, y) => x.rank - y.rank)
 
-                coin_data_sorted.forEach(val => {
-                    let quote = val.quotes.USD;
-                    let dot = {
-                        coin: val.name,
-                        x: quote.market_cap,
-                        y: quote.volume_24h,
-                        z: quote.percent_change_24h
-                    };
-                    let market_overview = {
-                        rank: val.rank,
-                        name: val.name,
-                        price: quote.price,
-                        price_change_24h: quote.percent_change_24h,
-                        market_cap: quote.market_cap,
-                        volume_24h: quote.volume_24h
-                    }
-                    // push item to liquidity_data
-                    liquidity_data.push(dot)
-                    // push item to market_overview
-                    market_overview_data.push(market_overview)
-                });
-
-
-                this.setState({
-                    market_overview_data: market_overview_data,
-                    liquidity_data: liquidity_data,
-                })
-            });
-    }
-
-    componentDidMount() {
-       this.fetchTickerData()
-    }
-
-    handleSelect = (event) => {
-       this.setState({
-           limit: event.target.value,
-       },()=> {
-           this.fetchTickerData()
-       })
-    }
+				coin_data_sorted.forEach(val => {
+					let quote = val.quotes.USD;
+					let dot = {
+						coin: val.name,
+						x: quote.market_cap,
+						y: quote.volume_24h,
+						z: quote.percent_change_24h
+					};
+					let market_overview = {
+						rank: val.rank,
+						name: val.name,
+						price: quote.price,
+						price_change_24h: quote.percent_change_24h,
+						market_cap: quote.market_cap,
+						volume_24h: quote.volume_24h
+					}
+					// push item to liquidity_data
+					liquidity_data.push(dot)
+					// push item to market_overview
+					market_overview_data.push(market_overview)
+				});
 
 
-    render() {
-        return (
-            <div>
-                <Router>
-                    <div>
-                        {/* header */}
-                        <Navbar>
-                            <Navbar.Header>
-                                <Navbar.Brand>
-                                    <a href="/">Top Coins</a>
-                                </Navbar.Brand>
-                                <Navbar.Toggle />
-                            </Navbar.Header>
-                            <Nav>
-                                {/* fix nested a tag issue: https://stackoverflow.com/questions/42561137/link-cannot-appear-as-a-descendant-of-a-link */}
-                                <NavItem componentClass='span' eventKey={1} href="#">
-                                    <Link to="/">Market Overview</Link>
-                                </NavItem>
-                                <NavItem componentClass='span' eventKey={2} href="#">
-                                    <Link to="/liquidity">Liquidity Analysis</Link>
-                                </NavItem>
-                            </Nav>
-                            <Nav pullRight>
-                            <NavItem>
-                                <p >Coin Limit</p>
+				this.setState({
+					market_overview_data: market_overview_data,
+					liquidity_data: liquidity_data,
+				})
+			});
+	}
+
+	componentDidMount() {
+		this.fetchTickerData()
+	}
+
+	handleSelect = (event) => {
+		this.setState({
+			limit: event.target.value,
+		}, () => {
+			this.fetchTickerData()
+		})
+	}
+
+
+	render() {
+		return (
+			<div>
+				<Router>
+					<div>
+						{/* header */}
+						<Navbar>
+							<Navbar.Header>
+								<Navbar.Brand>
+									<a href="/">Top Coins</a>
+								</Navbar.Brand>
+								<Navbar.Toggle />
+							</Navbar.Header>
+							<Nav>
+								{/* fix nested a tag issue: https://stackoverflow.com/questions/42561137/link-cannot-appear-as-a-descendant-of-a-link */}
+								<NavItem componentClass='span' eventKey={1} href="#">
+									<Link to="/">Market Overview</Link>
+								</NavItem>
+								<NavItem componentClass='span' eventKey={2} href="#">
+									<Link to="/liquidity">Liquidity Analysis</Link>
+								</NavItem>
+							</Nav>
+							<Nav pullRight>
+								<NavItem>
+									Coin Limit
                             </NavItem>
-                            <NavItem>
-                                <select onChange={this.handleSelect} value={this.state.limit}>
-                                    <option value="10">10</option>
-                                    <option value="50">50</option>
-                                    <option value="all">all</option>
-                                </select>
-                                </NavItem>
-                            </Nav>
-                        </Navbar>
-                        <Route exact path="/" render={(props) => <MarketOverview {...props} data={this.state.market_overview_data} />} />
-                        <Route path="/liquidity" render={(props) => <LiquidityAnalysis {...props} data={this.state.liquidity_data} />} />
-                    </div>
-                </Router>
-            </div>
-        );
-    }
+								<NavItem>
+									<select onChange={this.handleSelect} value={this.state.limit}>
+										<option value="10">10</option>
+										<option value="50">50</option>
+										<option value="all">all</option>
+									</select>
+								</NavItem>
+							</Nav>
+						</Navbar>
+						<Route exact path="/" render={(props) => <MarketOverview {...props} data={this.state.market_overview_data} />} />
+						<Route path="/liquidity" render={(props) => <LiquidityAnalysis {...props} data={this.state.liquidity_data} />} />
+					</div>
+				</Router>
+			</div>
+		);
+	}
 }
 
 export default App;
